6d7b9c343a4e01c8140bbac83705e5e865a9f1c6
Sietse Ringers <mail@sietseringers.net>
2018-03-07 12:04:13 +0100

    Proxy requests to backoffice

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config');
+const proxy = require('express-http-proxy');
 
 const app = express();
 app.use(bodyParser.text()); // TODO: restrict to one endpoint
@@ -8,6 +9,10 @@ app.use(bodyParser.json()); // TODO: restrict to one endpoint
 
 app.get('/api/get-participants', require('./actions/get-participants'));
 app.get('/api/get-signature/:bsn', require('./actions/get-signature'));
+
+app.use('/api/huishoudboekje', proxy('demo.api.huishoudboekje030.nl', {
+  https: false,
+}));
 
 const server = app.listen(config.port, () => {
   console.log(`Ketenpartner backend listening on port ${config.port}!`); // eslint-disable-line no-console
